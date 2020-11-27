6fd791f9ad862e0776c4ddd309518edda29ec599
genggeng <genggeng@163.com>
2020-11-27 15:03:28 +0800

    hot-fix commit

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -2,5 +2,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("change color");
+        System.out.println("hot-fix");
     }
 }
